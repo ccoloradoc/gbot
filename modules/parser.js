af9565d32dd1563875784869dcd6917857ec833d
Cristian Colorado <ccoloradoc@gmail.com>
2017-04-08 11:47:25 -0500

    Adding url cleaner

--- modules/parser.js
+++ modules/parser.js
@@ -2,6 +2,10 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 module.exports = {
+  clean: function(url) {
+    target = decodeURIComponent(url.replace('https://l.facebook.com/l.php?u=', ''));
+    return target.substring(0, target.indexOf('&'));
+  },
   extract: function(url, callback) {
     //console.log(url);
     var options =  {
@@ -16,15 +20,27 @@ module.exports = {
 
           $('img').filter(function() {
               var uri = '';
-              if(options.url.includes('wallgif.com') || options.url.includes('giphy.com/go')) {
+
+              // if(options.url.includes('wallgif.com') || options.url.includes('giphy.com/go')) {
+              //   console.log($(this))
+              //   uri = $(this).attr('data-gif');
+              // } else if(options.url.includes('gifsnation.com')) {
+              //   uri = $(this).attr('src');
+              // } else {
+              //   uri = $(this).attr('src');
+              //   if(uri.includes('http://www.phygee.com/') && !uri.includes('files/profile/')) {
+              //     uri = '';
+              //   }
+              // }
+
+              if($(this).attr('data-gif') != undefined) {
                 uri = $(this).attr('data-gif');
-              } else if(options.url.includes('gifsnation.com')) {
-                uri = $(this).attr('src');
               } else {
                 uri = $(this).attr('src');
-                if(uri.includes('http://www.phygee.com/') && !uri.includes('files/profile/')) {
-                  uri = '';
-                }
+              }
+
+              if(uri.includes('http://www.phygee.com/') && !uri.includes('files/profile/')) {
+                uri = '';
               }
 
               if(uri != undefined && uri.startsWith('http') && uri.endsWith('.gif')) {
