843cd50931bfa12853f45834b0b99e4253376e08
Cristian Colorado <cristian@wedevelop.mx>
2017-04-18 22:22:09 -0500

    Adding RabbitMQ credentials

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,8 @@
 if(process.env.NODE_ENV === 'production') {
   module.exports = {
     ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-    VERIFY_TOKEN: process.env.VERIFY_TOKEN
+    VERIFY_TOKEN: process.env.VERIFY_TOKEN,
+    MONGO_DB: process.env.MONGO_DB
   };
 } else {
   module.exports = require('./development.json');
