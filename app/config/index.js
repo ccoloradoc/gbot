911622913f7bd8eb91c5d1abafaf728cb87ad6ec
Cristian Colorado <cristian@wedevelop.mx>
2017-04-19 23:01:33 -0500

    PM2 Configuration

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,8 @@ if(process.env.NODE_ENV === 'production') {
   module.exports = {
     ACCESS_TOKEN: process.env.ACCESS_TOKEN,
     VERIFY_TOKEN: process.env.VERIFY_TOKEN,
-    MONGO_DB: process.env.MONGO_DB
+    MONGO_DB: process.env.MONGO_DB,
+    MQ_URL: process.env.MQ_URL
   };
 } else {
   module.exports = require('./development.json');
