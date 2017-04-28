fefccb6dbc551e9c7c454b4b0132ec5e243adc97
Cristian Colorado <ccoloradoc@gmail.com>
2017-04-28 13:11:54 -0500

    MongoStore: Session usage

--- app/server.js
+++ app/server.js
@@ -6,13 +6,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session      = require('express-session');
+var mongoose = require('mongoose');
+var config = require('./config')
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
 
+var connection = mongoose.createConnection(config.MONGO_DB);
+var MongoStore = require('connect-mongo')(session);
+
 var app = express();
 
-// view engine setup
+// view engine setupMONGO_DB
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,7 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(session({ secret: '4815162342s' }));
+
+app.use(session({
+  secret: '4815162342s',
+  store: new MongoStore({ mongooseConnection: connection })
+}));
 
 app.use('/', routes);
 app.use('/api', api);
