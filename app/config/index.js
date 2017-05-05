1433add8ed7ad753dd877a17d6c311a06df81990
Cristian Colorado <ccoloradoc@gmail.com>
2017-05-05 12:52:23 -0500

    Fix: Storage path to config

--- app/config/index.js
+++ app/config/index.js
@@ -5,7 +5,8 @@ if(process.env.NODE_ENV === 'production') {
     ACCESS_TOKEN: process.env.ACCESS_TOKEN,
     VERIFY_TOKEN: process.env.VERIFY_TOKEN,
     MONGO_DB: process.env.MONGO_DB,
-    MQ_URL: process.env.MQ_URL
+    MQ_URL: process.env.MQ_URL,
+    STORAGE_PATH: process.env.STORAGE_PATH
   };
 } else {
   module.exports = require('./development.json');
